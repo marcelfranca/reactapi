97a359b2f3a434f58bbae42e32942df38cef47ea
marcelfranca <Nan44aocw@#>
2020-01-25 14:37:53 -0300

    done

--- hunterweb/src/App.js
+++ hunterweb/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import Routes from './routes';
 
 import "./styles.css";
 
 import Header from './components/Header';
-import Main from "./pages/main";
+// import Main from "./pages/main";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes />
     </div>
   );
 }
